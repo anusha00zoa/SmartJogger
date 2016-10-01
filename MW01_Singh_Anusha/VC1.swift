aa4623768c81eea3577012d6abecaf2990c08ed5
anusha00zoa <anusha00zoa@gmail.com>
2016-09-30 21:13:15 -0500

    Adding functionality

--- MW01_Singh_Anusha/VC1.swift
+++ MW01_Singh_Anusha/VC1.swift
@@ -18,14 +18,17 @@ class VC1: UIViewController
     @IBOutlet weak var newLapButton: UIButton!
     @IBOutlet weak var showStatsButton: UIButton!
     
-    var startTime: Double = 0.0
-    var currentTime: Double = 0.0
-    var elapsedTime: Double = 0.0
-    var lapTime: Double = 0.0
-    var totalTime: Double = 0.0
-    var finishTime: Double = 0.0
+    var startTime = 0.0
+    var currentTime = 0.0
+    var elapsedTime = 0.0
+    var lapTime = 0.0
+    var totalTime = 0.0
     
     var numberOfLaps: Int = 0
+    
+    var jogTimer: Timer!
+    
+    public var lapData: [(String, String)] = []
     
     override func viewDidLoad()
     {
@@ -33,135 +36,89 @@ class VC1: UIViewController
         
         newLapButton.isEnabled = false
         showStatsButton.isEnabled = false
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-    func UpdateTimeForCurrentLap()
+    func ConvertToFormat(time: Double) -> String
     {
-        print("New Lap Started at " + String(startTime))
+        var strTime: String = ""
+        
+        var pseudoTime: Double = time
+        
+        let timeInMin = UInt8(pseudoTime / 60.0)
+        let strTimeInMin = String(format: "%1d", timeInMin);
+        
+        pseudoTime -= (TimeInterval(timeInMin) * 60)
+
+        let timeInSec = UInt8(pseudoTime)
+        let strTimeInSec = String(format: "%02d", timeInSec);
+
+        pseudoTime -= TimeInterval(timeInSec)
+        
+        let timeInMilliSec = UInt8(pseudoTime * 10)
+        let strTimeInMilliSec = String(format: "%1d", timeInMilliSec);
+        
+        strTime = "\(strTimeInMin):\(strTimeInSec):\(strTimeInMilliSec)"
+        
+        return strTime
+    }
+    
+    func TimeTakenForCurrentLap()
+    {
         currentTime = NSDate.timeIntervalSinceReferenceDate
         
         elapsedTime = currentTime - startTime
-        totalTime += elapsedTime;
+        totalTime = elapsedTime;
         
-        currentLapLabel.text = "Current Lap: " + String(elapsedTime)
-        totalTimeLabel.text = "Total Time: " + String(totalTime)
-    }
-    
-    func StartNewLap()
-    {
-        startTime = NSDate.timeIntervalSinceReferenceDate
-        UpdateTimeForCurrentLap()
+        currentLapLabel.text = "Current Lap: " + ConvertToFormat(time: elapsedTime)
+        totalTimeLabel.text = "Total Time: " + ConvertToFormat(time: totalTime)
         
     }
     
     @IBAction func StartButtonClicked(_ sender: AnyObject)
     {
-        if(startStopToggleButton.title(for: UIControlState.normal) == "Start")
+        if(startStopToggleButton.title(for: UIControlState.normal) == "START")
         {
-            startStopToggleButton.setTitle("Stop", for: UIControlState.normal)
+            startStopToggleButton.setTitle("STOP", for: .normal)
+            
             newLapButton.isEnabled = true
-            
-            StartNewLap()
-            
-            numberOfLaps += 1
+            newLapButton.backgroundColor = UIColor.lightGray.withAlphaComponent(0.75)
+            numberOfLaps = 1
             numberOfLapsLabel.text = "Number Of Laps: " + String(numberOfLaps)
+            
+            let sel : Selector = #selector(VC1.TimeTakenForCurrentLap)
+            jogTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: sel, userInfo: nil, repeats: true)
+            startTime = NSDate.timeIntervalSinceReferenceDate
         }
         else
         {
-            startStopToggleButton.setTitle("Start", for: UIControlState.normal)
+            startStopToggleButton.setTitle("START", for: .normal)
+            startStopToggleButton.isEnabled = false
+            startStopToggleButton.backgroundColor = UIColor.darkGray
+
+            newLapButton.isEnabled = false
+            newLapButton.backgroundColor = UIColor.darkGray
+            
+            showStatsButton.isEnabled = true
+            showStatsButton.backgroundColor = UIColor.lightGray.withAlphaComponent(0.75)
+            
+            jogTimer.invalidate()
+            
         }
     }
     
     @IBAction func NewLapButtonClicked(_ sender: AnyObject)
     {
-        
+        numberOfLaps +=  1
+        numberOfLapsLabel.text = "Number Of Laps: " + String(numberOfLaps)
     }
     
     @IBAction func ShowStatsButtonClicked(_ sender: AnyObject)
     {
         
     }
-    
-    
-    // MARK: - Table view data source
-
-    /*override func numberOfSections(in tableView: UITableView) -> Int
-    {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
-    {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }*/
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
